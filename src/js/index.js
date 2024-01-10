cc5fd939a581545956c3350805700e98887f3fe9
Cauê Brolese <cauebrolesef@gmail.com>
2024-01-09 22:10:36 -0300

    refactoring js

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,21 @@
-document.querySelector('.form').addEventListener('submit', event => {
-    event.preventDefault()
-})
-
 const inputs = document.querySelectorAll('.input')
 const submit = document.querySelector('.submit')
-const requiredField = document.querySelectorAll('.required-field')
 
-removeClassError()
-
-inputs.forEach((input, index) => {
-    submit.addEventListener('click', () => {
-        if (input.value === "") {
-            input.classList.remove('green-border')
-            input.classList.add('red-border')
-            requiredField[index].classList.add('visible')
+submit.addEventListener('click', e => {
+    e.preventDefault()
+    
+    inputs.forEach((input) => {
+        if (input.value) {
+            input.classList.add('valid')
+            input.nextElementSibling.classList.remove('visible')
+            submit.classList.remove('error')
+            console.log(input);
         } else {
-            input.classList.remove('red-border')
-            input.classList.add('green-border')
-            requiredField[index].classList.remove('visible')
-        }
-
-        if (inputs[3].classList.contains('red-border')){
+            input.classList.remove('valid')
+            input.classList.add('invalid')
+            input.nextElementSibling.classList.add('visible')
             submit.classList.add('error')
-        }else {
-            submit.classList.remove('error')        
+            console.log(input);
         }
     })
 })
-
-function removeClassError() {
-    submit.classList.remove('error')
-}
